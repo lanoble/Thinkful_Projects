63b9ee77cbc666b307e0657dc0cc567718ad6a0c
Lewis Noble <lewis.a.noble@gmail.com>
2015-02-08 22:29:38 -0500

    Create linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm 
+
+loansData = pd.read_csv('loansData.csv')
+
+loansData['Interest.Rate'] = map((lambda x: x[:len(x)-1]),loansData['Interest.Rate'])
+loansData['Loan.Length'] = map((lambda x: x[:2]),loansData['Loan.Length'])
+loansData['FICO.Range'] = map((lambda x: str(x).split('-')),loansData['FICO.Range'])
+loansData['FICO.Score'] = map((lambda x: x[0]),loansData['FICO.Range'])
+
+loansData['Interest.Rate'] = loansData['Interest.Rate'].astype(float)
+loansData['Loan.Length'] = loansData['Loan.Length'].astype(float)
+loansData['FICO.Score'] = loansData['FICO.Score'].astype(float)
+
+#extract columns
+intrate = loansData['Interest.Rate']
+loanamt = loansData['Amount.Requested']
+fico = loansData['FICO.Score']
+
+#reshape depent variable data
+y = np.matrix(intrate).transpose()
+
+#reshape indepent variables shaped as columns
+x1 = np.matrix(fico).transpose()
+x2 = np.matrix(loanamt).transpose()
+
+#put two columns together to create an input matrix
+x = np.column_stack([x1,x2])
+
+#create a linear model
+X = sm.add_constant(x)
+model = sm.OLS(y,X)
+f = model.fit()
+
+print 'Coefficients: ', f.params[1:3]
+print 'Intercept: ', f.params[0]
+print 'P-Values: ', f.pvalues 
+print 'R-Squared : ', f.rsquared 
+
