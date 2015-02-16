b07172341fe0d46a063ab20550ba0e0cb2f2e80e
Lewis Noble <lewis.a.noble@gmail.com>
2015-02-15 22:33:40 -0500

    Update time_series.py

--- time_series.py
+++ time_series.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
-df = pd.read_csv('LoansStats3B.csv', header=1, low_memory = False)
-df = df.drop(df.index[188122:])
+df = pd.read_csv('LoansStats3b.csv', header=1, low_memory = False)
+df.drop(df.index[188122:])
 
 df['issue_d_format'] = pd.to_datetime(df['issue_d'])
 dfts = df.set_index('issue_d_format')
 year_month_summary = dfts.groupby(lambda x: x.year * 100 + x.month).count()
-year_month_summary.index = pd.to_datetime(year_month_summary.index, format=%Y%m)
+year_month_summary.index = pd.to_datetime(year_month_summary.index, format='%Y%m')
+loan_count_summary = year_month_summary['issue_d']
 
 plt.figure()
 loan_count_summary.plot()
